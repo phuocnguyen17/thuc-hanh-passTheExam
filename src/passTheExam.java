6e768bc4302bdf82610c156b1ff1069f33559a45
thanhnguyenquoc15 <thanhnguyenquoc15@gmail.com>
2023-03-24 10:58:27 +0700

    thuc-hanh

---
+++ src/passTheExam.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class passTheExam {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner sc = new Scanner(System.in);
+        do {
+            size = sc.nextInt();
+            if (size < 30)
+                System.out.println("Size should not exceed 30");
+        }while (size >30);
+        array = new int[size];
+        int i =0;
+        while(i< array.length){
+            System.out.println("Enter a mark for student"+(i+1)+":");
+            array[i] = sc.nextInt();
+            i++;
+        }
+        int count = 0;
+        System.out.println(" List of mark: ");
+        for (int j=0; j<array.length; j++){
+            System.out.println(array[j]+"\t");
+            if (array[j]>=5 && array[j]<10);
+            count++;
+        }
+        System.out.println("\n The number of students passing the exam is " + count);
+    }
+}
